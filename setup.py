bb175b8027ba1a487fa80145411e363a3a738676
Bart Thate <bthate67@gmail.com>
2022-08-12 15:23:22 +0200

    up version

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ def uploadlist(dir):
 
 setup(
     name='president',
-    version='23',
+    version='30',
     url='https://bitbucket.org/bthate/president',
     author='Bart Thate',
     author_email='bthate@dds.nl',
