1cb65abea5f1ddda0c8e469c2dbf4c11cd032ff2
B.H.J. Thate <thatebhj@gmail.com>
2023-01-08 04:24:13 +0100

    up version

--- setup.py
+++ setup.py
@@ -62,11 +62,11 @@ def uploadlist(dir):
 
 setup(
     name='president',
-    version='100',
+    version='101',
     url='https://bitbucket.org/bthate/president',
     author='Bart Thate',
     author_email='bthate@dds.nl',
-    description="Prosecutor. Court. Reconsider OTP-CR-117/19.",
+    description="@KarimKhanQC reconsider OTP-CR-117/19",
     license='Public Domain',
     packages=["president"],
     include_package_data=True,
